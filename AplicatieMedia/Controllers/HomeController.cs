49d9cf27d4a78657196eeb2c815dbdfcde31540e
Evgheni Pirvan <eparvan@gmail.com>
2019-02-09 09:00:51 +0200

    Add project files.

---
+++ AplicatieMedia/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicatieMedia.Models;
+
+namespace AplicatieMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new Numere());
+        }
+        [HttpPost]
+        public ActionResult Index(Numere Numere)
+        {
+            return View(Numere);
+        }
+        [HttpPost]
+        public ActionResult CreateNumere(Numere Numere)
+        {
+            
+            double m = (double)((Numere.Numar1 + Numere.Numar2 + Numere.Numar3)) / 3;
+            
+            string rez = "(" + Numere.Numar1.ToString()+ " + "+ Numere.Numar3.ToString() + " + " + Numere.Numar3.ToString() + ")" + "/" + "3" + "=" + m.ToString("F3");
+            ViewBag.media = rez;
+            return View();
+        }
+        
+       
+
+    }
+}
---
+++ AplicatieMedia/Models/Numere.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicatieMedia.Models
+{
+    public class Numere
+    {
+        public int Numar1 { get; set; }
+        public int Numar2 { get; set; }
+        public int Numar3 { get; set; }
+        public double Media { get; set; }
+    }
+}
